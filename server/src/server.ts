e0ad9601b92d5c591c77acccaf88c36cb35aae93
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-09 11:57:54 -0300

    update cors position

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,8 +11,8 @@ import cors from "cors";
 const app = express();
 app.listen(3000, () => console.log("Server is running on port 3000"));
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use("/images", express.static(UPLOADS_FOLDER));
 app.use(routes);
 
