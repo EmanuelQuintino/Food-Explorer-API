48bd8439920b0371e1c5e7d7edde592073fb30e8
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-10 11:14:51 -0300

    add food explorer api

--- client/src/pages/PageError/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./style"
-
-export function PageError() {
-  return (
-    <Container>
-      <h1>Error 404...</h1>
-    </Container>
-  )
-}
--- client/src/pages/PageError/style.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.section`
-  h1 {
-    color: ${({ theme }) => theme.colors.cake200};
-  }
-`;
