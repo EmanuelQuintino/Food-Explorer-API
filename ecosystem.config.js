6b9d85edf7737c8d4450f649c0f62a721e74158e
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-14 18:55:37 -0300

    add pm2 start

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [{
+    name: "app",
+    script: "dist/server.js",
+    instances: 1,
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    },
+  }],
+};
