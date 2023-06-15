41962d86024791f3b8ad8d47e406aed17d65f8e2
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-15 17:58:08 -0300

    update ecosystem config instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "food-explorer-api",
     script: "dist/server.js",
-    instances: "max",
+    instances: 1,
     env: {
       NODE_ENV: "development",
     },
