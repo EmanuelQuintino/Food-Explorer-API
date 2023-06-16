b70f285bf9a27672cd660a80c707dd41e5f225c8
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-16 12:17:34 -0300

    add ecosystem config watch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,8 @@ module.exports = {
     name: "food-explorer-api",
     script: "dist/server.js",
     instances: "max",
+    watch: true,
+    ignore_watch: ["node_modules", "tmp", "uploads"],
     env: {
       NODE_ENV: "development",
     },
